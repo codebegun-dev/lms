0b9ff635fb3198f760ab6b34f8155151ebe292b8
Narasimhanaidu <narasimha469@gmail.com>
2025-10-16 12:56:18 +0530

    first commit frontend

--- clinet/CoreDemoProject/src/com/sample/test/coreprograms/Space.java
+++
@@ -1,9 +0,0 @@
-package com.sample.test.coreprograms;
-
-public class Space {
-	public static void main(String args[]) {
-		String name="Java Script";
-	System.out.println(name.replaceAll("\\s",""));
-	}
-
-}
