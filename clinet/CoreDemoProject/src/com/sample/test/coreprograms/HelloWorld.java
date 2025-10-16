9a826774183169356ab54361aa12975f4f27814e
Narasimhanaidu <narasimha469@gmail.com>
2025-10-16 12:56:18 +0530

    first commit frontend

--- clinet/CoreDemoProject/src/com/sample/test/coreprograms/HelloWorld.java
+++
@@ -1,9 +0,0 @@
-package com.sample.test.coreprograms;
-
-public class HelloWorld {
-
-	public static void main(String[] args) {
-		System.out.println("Hello World");
-	}
-
-}
--- clinet/CoreDemoProject/src/com/sample/test/demo/HirInputA.java
+++
@@ -1,8 +0,0 @@
-package com.sample.test.demo;
-
-public class HirInputA {
-public int a=436;
-public void h1() {
-	System.out.println("Hierarchial Input A----");
-}
-}
