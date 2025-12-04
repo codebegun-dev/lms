c4815825f5ec4e0b10706e923a81d044f7f59e08
VenigandlaVineela <venigandlavineela10@gmail.com>
2025-12-04 11:36:46 +0530

    updated app.jsx

--- clinet/src/App.jsx
+++ clinet/src/App.jsx
@@ -36,7 +36,7 @@ import CreateTopic from './Admin_Dashboard/CreateTopic';
 import CreateSubTopic from './Admin_Dashboard/CreateSubTopic'; 
 
 //User Management
-import UserManagement from './Admin_Dashboard/UserManagement';
+import UserManagement from './Admin_Dashboard/UserManagement/UserManagement';
 
 //Manage Roles
 import ManageRoles from './Admin_Dashboard/ManageRoles';
@@ -45,6 +45,9 @@ import ManageRoles from './Admin_Dashboard/ManageRoles';
 import Syllabus from './Admin_Dashboard/SyllabusManagment/Syllabus';
 import Courses from './Admin_Dashboard/SyllabusManagment/Courses';
 import EditArticle from './Admin_Dashboard/SyllabusManagment/EditArticle';
+
+// Quiz Management
+import QuizForm from './Admin_Dashboard/QuizManagement/Quiz/QuizForm';
 
 // Sales Dashboard
 import SalesForm from './Sales_Dashboard/SalesForm';
@@ -58,8 +61,7 @@ import CreateCompaign from './Sales_Dashboard/CreateCompaign';
 // Sales Counselor Dashboard
 import SalesCounselorDashboard from './Sales_Dashboard/Sales_Counselor/SalesCounselorDashboard';
 import CounselorProfile from "./Sales_Dashboard/Sales_Counselor/CounselorProfile";
-import QuizForm from './Admin_Dashboard/QuizManagement/Quiz/QuizForm';
-
+ 
 
 function App() {
   return (
